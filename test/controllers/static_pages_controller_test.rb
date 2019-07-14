f16a8bc91b6d74d317de0115b9de814eab834e45
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-13 20:39:38 -0500

    removes base_title from home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Tweeter on Rails"
   end
 
   test "should get help" do
